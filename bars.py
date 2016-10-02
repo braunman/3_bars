c9503e6568463801bc9ff5d29ded48179d7715fc
Andrey Braun <braunman@gmail.com>
2016-10-02 15:44:23 +0000

    test

--- bars.py
+++ bars.py
@@ -1,21 +1,47 @@
 import json
 
+file = "bars.json"
 
 def load_data(filepath):
-    pass
+    url = 'http://data.mos.ru/opendata/7710881420-bary'
+    with open(file) as data_file:    
+        data = json.load(data_file)
+    return data
 
 
 def get_biggest_bar(data):
-    pass
+    print("Самый большой бар:")
+    big = 0 
+    bar = []
+    for i in data:
+        seats = i['Cells']["SeatsCount"]
+        if big <= seats or not big:
+            big = seats
+            bar = i['Cells']
+    print (bar['Name'], bar['Address'], bar['SeatsCount'], 'мест')
 
 
 def get_smallest_bar(data):
-    pass
+    print("Самый маленький бар:")
+    big = None
+    bar = []
+    for i in data:
+        seats = i['Cells']["SeatsCount"]
+        if not big:
+            big = seats
+        if big >= seats:
+            big = seats
+            bar = i['Cells']
+    print (bar['Name'], bar['Address'], bar['SeatsCount'], 'мест')
+
 
 
 def get_closest_bar(data, longitude, latitude):
+    
     pass
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data(file)
+    get_biggest_bar(data)
+    get_smallest_bar(data)
