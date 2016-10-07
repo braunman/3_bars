9ca51e85f51bda55220653243a1e70888d581550
braunman <braunman@gmail.com>
2016-10-07 17:21:09 +0300

    sdfs

--- bars.py
+++ bars.py
@@ -11,20 +11,17 @@ def load_data(filepath):
 
 
 def get_biggest_bar(data):
-    print("Самый большой бар:\n", 
-        max(data, key = lambda x: x['Cells']["SeatsCount"]), end = "\n"*2 )
+    return max(data, key = lambda x: x['Cells']["SeatsCount"])
 
 
 def get_smallest_bar(data):
-    print ("Самый маленький бар:\n", 
-        min(data, key = lambda x: x['Cells']["SeatsCount"]), end = "\n"*2 )
+    return min(data, key = lambda x: x['Cells']["SeatsCount"])
 
 
 
 def get_closest_bar(data, longitude, latitude):
-    print ("Самый близкий бар:\n", 
-        min(data, key = lambda x: vincenty((longitude, latitude),
-            x['Cells']['geoData']['coordinates']).meters), end = "\n"*2 )
+    return min(data, key = lambda x: vincenty((longitude, latitude),
+            x['Cells']['geoData']['coordinates']).meters)
 
 
 if __name__ == '__main__':
@@ -37,8 +34,8 @@ if __name__ == '__main__':
         print ("Вы не передали скрипту файл с барами")
         exit(1)
     bars_data = load_data(json_bars_file)
-    get_biggest_bar(bars_data)
-    get_smallest_bar(bars_data)
+    print("Самый большой бар:\n{}" .format(get_biggest_bar(bars_data)), end = "\n"*2 )
+    print ("Самый маленький бар:\n{}".format(get_smallest_bar(bars_data)), end = "\n"*2 )
     print ("Введите координаты по одной( в формате - 39.635709999611 ):")
     try:
         northern_latitude = float(input("северная широта:"))
@@ -46,4 +43,7 @@ if __name__ == '__main__':
     except:
         print("Не верно введены координаты, попробуйте в формате 39.6357099")
         exit(1)
-    get_closest_bar(bars_data, northern_latitude , eastern_longitude)
+    print ("Самый близкий бар:\n{}".format(
+        get_closest_bar(bars_data, northern_latitude , eastern_longitude)), 
+    end = "\n"*2 )
+
